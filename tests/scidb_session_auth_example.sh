fcff108eb712c42c977844a07d2f92779a50bf37
Rares Vernica <rvernica@gmail.com>
2017-10-25 22:31:06 -0700

    Clean-up output format in test scripts
    * Print operator used in each step

--- tests/scidb_session_auth_example.sh
+++ tests/scidb_session_auth_example.sh
@@ -14,6 +14,7 @@ api_url="https://${host}:${port}"
 curl_cmd="curl --silent --show-error --insecure"
 
 
+echo "list..."
 id=$($curl_cmd "$api_url/new_session?user=${USER}&password=${PASSWORD}")
 $curl_cmd "$api_url/execute_query?id=${id}&query=list('instances')&save=dcsv"
 $curl_cmd "$api_url/read_lines?id=${id}&n=0"
@@ -23,36 +24,45 @@ echo
 
 # Test the new prefix option to issue multiple commands in a single connection
 # A namespace example. Part 1: Create namespace 'cazart'
+echo "create_namespace..."
 id=$($curl_cmd "$api_url/new_session?user=${USER}&password=${PASSWORD}")
 $curl_cmd "$api_url/execute_query?id=${id}&query=create_namespace('cazart')&release=1" > /dev/null
-echo "create_namespace('cazart')"
+echo
 
 
 # Add an array to the cazart namespace
+echo "store..."
 id=$($curl_cmd "$api_url/new_session?user=${USER}&password=${PASSWORD}")
 $curl_cmd "$api_url/execute_query?id=${id}&prefix=set_namespace('cazart')&query=store(list(),yikes)&release=1" > /dev/null
+echo
 
 
 # context (list the contents of the 'cazart' namespace)
+echo "list..."
 id=$($curl_cmd "$api_url/new_session?user=${USER}&password=${PASSWORD}")
 $curl_cmd "$api_url/execute_query?id=${id}&prefix=set_namespace('cazart')&query=list()&save=dcsv" > /dev/null
 $curl_cmd "$api_url/read_lines?id=${id}&n=0"
 $curl_cmd "$api_url/release_session?id=${id}"
+echo
 
 
 #It's good to clean up after yourself
+echo "remove..."
 id=$($curl_cmd "$api_url/new_session?user=${USER}&password=${PASSWORD}")
 $curl_cmd "$api_url/execute_query?id=${id}&query=remove(cazart.yikes)" > /dev/null
 $curl_cmd "$api_url/release_session?id=${id}"
+echo
 
+
+echo "drop_namespace..."
 id=$($curl_cmd "$api_url/new_session?user=${USER}&password=${PASSWORD}")
 $curl_cmd "$api_url/execute_query?id=${id}&query=drop_namespace('cazart')" > /dev/null
 $curl_cmd "$api_url/release_session?id=${id}"
+echo
 
 
 #Test the negative case:
 PASSWORD=wrong
-echo
 echo "You should see an authentication error here:"
 $curl_cmd "$api_url/new_session?user=${USER}&password=${PASSWORD}"
 echo
