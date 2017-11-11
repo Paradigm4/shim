638805787aa5934953a9fb2205757a67ced55db5
Rares Vernica <rvernica@gmail.com>
2017-11-10 22:04:57 -0800

    Strip output

--- tests/tls_scidbauth.sh
+++ tests/tls_scidbauth.sh
@@ -23,8 +23,8 @@ openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 -subj "/C=US/ST=MA/L=W
 sleep 1
 
 id=$(curl -f -s -k "https://${host}:${port}/new_session" | sed -e "s/.*//")
-curl -f -k "https://${host}:${port}/execute_query?id=${id}&query=list('functions')&save=dcsv&user=root&password=Paradigm4" || fail
-curl -f -k "https://${host}:${port}/read_lines?id=${id}&n=0" || fail
+curl -s -f -k "https://${host}:${port}/execute_query?id=${id}&query=list('functions')&save=dcsv&user=root&password=Paradigm4" > /dev/null || fail
+curl -s -f -k "https://${host}:${port}/read_lines?id=${id}&n=0" > /dev/null || fail
 curl -f -k "https://${host}:${port}/release_session?id=${id}" || fail
 echo "OK"
 
