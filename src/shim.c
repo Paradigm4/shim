3f34b71e3e82e815aa05e6e3ea168ba19567fc01
Rares Vernica <rvernica@gmail.com>
2017-11-10 22:08:20 -0800

    Remove left-over code from #67

--- src/shim.c
+++ src/shim.c
@@ -1246,10 +1246,6 @@ execute_query (struct mg_connection *conn, const struct mg_request_info *ri)
                free (prefix);
                syslog (LOG_ERR, "execute_query error on prefix %s", SERR);
                respond (conn, plain, 500, strlen (SERR), SERR);
-               //if (s->con)
-               //  scidbdisconnect (s->con);
-               //s->con = NULL;
-               //cleanup_session (s);
                omp_unset_lock (&s->lock);
                return;
              }
@@ -1268,10 +1264,6 @@ execute_query (struct mg_connection *conn, const struct mg_request_info *ri)
                free (prefix);
                syslog (LOG_ERR, "execute_prepared_query error on prefix %s", SERR);
                respond (conn, plain, 500, strlen (SERR), SERR);
-               //if (s->con)
-               //  scidbdisconnect (s->con);
-               //s->con = NULL;
-               //cleanup_session (s);
                omp_unset_lock (&s->lock);
                return;
             }
@@ -1290,10 +1282,6 @@ execute_query (struct mg_connection *conn, const struct mg_request_info *ri)
       free (prefix);
       syslog (LOG_ERR, "execute_query error %s", SERR);
       respond (conn, plain, 500, strlen (SERR), SERR);
-      //if (s->con)
-      //  scidbdisconnect (s->con);
-      //s->con = NULL;
-      //cleanup_session (s);
       omp_unset_lock (&s->lock);
       return;
     }
@@ -1321,10 +1309,6 @@ execute_query (struct mg_connection *conn, const struct mg_request_info *ri)
       syslog (LOG_ERR, "execute_prepared_query error %s", SERR);
       if (!stream)
         respond (conn, plain, 500, strlen (SERR), SERR);
-      //if (s->con)
-      //  scidbdisconnect (s->con);
-      //s->con = NULL;
-      //cleanup_session (s);
       omp_unset_lock (&s->lock);
       return;
     }
