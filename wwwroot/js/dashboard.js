dfd242bc2c7c0297814e7de472c9a7d3917bed64
Rares Vernica <rvernica@gmail.com>
2018-08-27 20:34:35 -0700

    List instances, refresh, and cancel query
    * List instances in second table
    * Use one Shim session per table
    * Add refresh button for each table
    * Add cancel button for each query row

--- wwwroot/js/dashboard.js
+++ wwwroot/js/dashboard.js
@@ -1,12 +1,27 @@
 var baseURL = 'http://localhost:8080';
 
-var sessionID = undefined;
-var statusElement = document.getElementById('table_foot');
+var sessionQue = undefined;
+var sessionIns = undefined;
 
-status.textContent = 'Loading...';
+var tableQue = document.getElementById('que_body');
+var tableIns = document.getElementById('ins_body');
+
+var statusQue = document.getElementById('que_foot');
+var statusIns = document.getElementById('ins_foot');
+
+statusQue.textContent = 'Loading...';
+statusIns.textContent = 'Loading...';
 
 
-function shimRequest(endpoint, args, requestHandler) {
+function setDisabledClass(className, value) {
+    [].forEach.call(
+        document.getElementsByClassName(className),
+        function(el) {
+            el.disabled = value;
+        });
+}
+
+function shimRequest(sessionID, endpoint, args, status, requestHandler) {
     var request = new XMLHttpRequest();
 
     var url = baseURL + '/' + endpoint + '?';
@@ -21,36 +36,20 @@ function shimRequest(endpoint, args, requestHandler) {
 
     request.open('GET', url, true);
 
-    request.onload = function() {
-        if (request.status >= 200 && request.status < 400) {
-            requestHandler(request.responseText);
-        } else {
-            status.textContent = 'Shim connection error!';
-        }
-    };
+    request.addEventListener(
+        'load',
+        function() {
+            requestHandler(this.status, this.responseText);
+        });
 
-    request.onerror = function() {
-        status.textContent = 'Shim connection error!';
-    };
+    request.addEventListener(
+        'error',
+        function() {
+            status.textContent = 'Shim connection error!';
+        });
 
     request.send();
 }
-
-
-function shimList() {
-    shimRequest(
-        'execute_query',
-        "query=filter(list('queries'),query_string<>'')&save=csv%2B",
-        function(data) {
-            console.log(data);
-            shimRequest('read_lines', undefined, function(data) {
-                console.log(data);
-                parseResult(data)
-            });
-        });
-}
-
-var l;
 
 function parseResult(data) {
     var lines = data.split(/\n/);
@@ -60,7 +59,6 @@ function parseResult(data) {
             return;
         }
         console.log(line);
-        l=line;
         var rec = [];
         var re_token = /(?:^|,)(?:(?:'((?:(?:\\')|[^'])*)')|([^',]*))/g;
         while ((token = re_token.exec(line)) !== null) {
@@ -88,9 +86,153 @@ function parseResult(data) {
     return records;
 }
 
+function populateTable(results, table, postProcess) {
+    while (table.firstChild)
+        table.removeChild(table.firstChild);
+    results.forEach(function(result) {
+        var tr = document.createElement('tr');
+        result.forEach(function(value) {
+            var td = document.createElement('td');
+            td.textContent = value;
+            tr.appendChild(td);
+        });
+        if (typeof postProcess !== 'undefined')
+            postProcess(tr);
+        table.appendChild(tr);
+    });
+}
 
-shimRequest('new_session', undefined, function(data) {
-    sessionID = data;
-    console.log('Session ID: ' + sessionID);
-    shimList();
-});
+function shimQuery(
+    sessionID, query, status, table, postProcess, finalize) {
+    status.textContent = 'Loading...';
+    shimRequest(
+        sessionID,
+        'execute_query',
+        'query=' + query + '&save=csv%2B',
+        status,
+        function(code, data) {
+            console.log(data);
+            if (code !== 200) {
+                status.textContent = 'Shim error! ' + code;
+                return;
+            }
+
+            shimRequest(
+                sessionID,
+                'read_lines',
+                undefined,
+                status,
+                function(code, data) {
+                    console.log(data);
+                    if (code !== 200) {
+                        status.textContent = 'Shim error! ' + code;
+                        return;
+                    }
+
+                    populateTable(parseResult(data), table, postProcess);
+                    status.textContent = 'Ready.';
+                    if (typeof finalize !== 'undefined')
+                        finalize();
+                });
+        });
+}
+
+function getQueries() {
+    setDisabledClass('que', true);
+    shimQuery(
+        sessionQue,
+        "filter(list('queries'),query_string<>'')",
+        statusQue,
+        tableQue,
+        function(tr) {
+            var qid = tr.childNodes[2].textContent;
+
+            var inp = document.createElement('input');
+            inp.value = 'Cancel';
+            inp.type = 'button';
+            inp.onclick = function() {cancelQuery(qid);};
+            inp.classList.add('que');
+
+            var td = document.createElement('td');
+            td.appendChild(inp);
+
+            tr.insertBefore(td, tr.firstChild);
+            return tr;
+        },
+        function() {
+            setDisabledClass('que', false);
+        });
+}
+
+function getInstances() {
+    shimQuery(
+        sessionIns,
+        "list('instances')",
+        statusIns,
+        tableIns,
+        undefined,
+        undefined);
+}
+
+function cancelQuery(qid) {
+    setDisabledClass('que', true);
+    statusQue.textContent = 'Cancelling...';
+    shimRequest(
+        sessionQue,
+        'execute_query',
+        "query=cancel('" + qid + "')",
+        statusQue,
+        function(code, data) {
+            console.log(data);
+            if (code === 200 ||
+                code === 406 &&
+                data.indexOf('Query ' + qid + ' not found') !== -1) {
+                statusQue.textContent = 'Ready.';
+                setDisabledClass('que', false);
+                getQueries();
+            } else {
+                statusQue.textContent = 'Shim error! ' + code;
+                return;
+            }});
+}
+
+function initialize() {
+    shimRequest(
+        undefined,
+        'new_session',
+        'admin=1',
+        statusQue,
+        function(code, data) {
+            console.log(data);
+            if (code !== 200) {
+                statusQue.textContent = 'Shim error! ' + code;
+                return;
+            }
+
+            sessionID = data;
+            console.log('Session ID: ' + sessionID);
+            sessionQue = sessionID;
+            getQueries();
+        });
+
+    shimRequest(
+        undefined,
+        'new_session',
+        undefined,
+        statusIns,
+        function(code, data) {
+            console.log(data);
+            if (code !== 200) {
+                statusIns.textContent = 'Shim error! ' + code;
+                return;
+            }
+
+            sessionID = data;
+            console.log('Session ID: ' + sessionID);
+            sessionIns = sessionID;
+            getInstances();
+            document.getElementById('ins_button').disabled=false;
+        });
+}
+
+initialize();
